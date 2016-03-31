9727baddf437f4b21b73edf3d18f25bd90e40e5f
taktod <poepoemix@hotmail.com>
2016-03-31 19:38:47 +0900

    Initial Commit

---
+++ rtmpHologram/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  rtmpHologram
+//
+//  Created by taktod on 2016/03/31.
+//  Copyright © 2016年 taktod. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
